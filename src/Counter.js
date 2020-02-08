21716df3a2d6e6a4783d823d9a2cc69c99f73c94
sejin0004 <leesejin0004@gmail.com>
2020-02-08 16:02:28 +0900

    Refactoring counter using useReducer-Hooks

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+};
 
 export default () => {
-  const [number, setNumber] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
 
   return (
     <div>
-      <h1>{number}</h1>
-      <button onClick={() => setNumber(number + 1)}>+</button>
-      <button onClick={() => setNumber(number - 1)}>-</button>
+      <h1>{state.value}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
     </div>
   );
 };
